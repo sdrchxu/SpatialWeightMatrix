f62ce5d32634e4f67a39728caa3485d20d65761d
sdrchxu <sdrchxu@126.com>
2024-03-06 22:23:15 +0800

    尝试编写了多线程版本的程序，但仍有bug未解决

--- test_20240306.py
+++ test_20240306.py
@@ -1,8 +1,8 @@
 import sys
-import cal_moran_2
+import cal_moran_3
 
 
 shp_path="F:\大创数据\中间产出的数据\云南省和黄淮海平原已处理好的火点\云南省逐月火点\云南省已处理好的火点_3月.shp"
 field="BRIGHT_T31"
-output_file="D:\Lenovo\Desktop\云南大学\大创\程序代码\空间权重矩阵测试\云南省3月火点（阈值）"
-cal_moran_2.global_moran(shp_path,field,output_file,distance_function='inverse',threshold=55000,std=True)
+output_file="D:\Lenovo\Desktop\云南大学\大创\程序代码\空间权重矩阵测试\云南省3月火点（阈值，多线程）"
+cal_moran_3.global_moran(shp_path,field,output_file,distance_function='inverse',threshold=55000,std=True)
